2f0d036f2fb1e38c920b3a9a21bbf6bfd8f3d8c5
Bernardo Biesseck <biesseck@gmail.com>
2021-10-05 18:29:31 -0400

    Remoção de imports desnecessários no arquivo 'App.js'

--- App.js
+++ App.js
@@ -1,48 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import TelaLogin from './src/screens/TelaLogin';
-import TelaInicial from './src/screens/TelaInicial';
 
 import Routes from './src/navigation';
 
 export default function App() {
 
   return (
-
     <Routes/>
-  
   );
 
-
-  /*
-  const Stack = createNativeStackNavigator();
-
-  return (
-    <NavigationContainer>
-
-      <Stack.Navigator>
-      
-        <Stack.Screen
-          name="TelaLogin"
-          component={TelaLogin}
-          options={{ title: 'Título da Tela de Login'}}
-        />
-
-        <Stack.Screen
-          name="TelaInicial"
-          component={TelaInicial}
-          options={{ title: 'Título da Tela Inicial'}}
-        />
-      
-      </Stack.Navigator>
-      
-    </NavigationContainer>
-  );
-  */
 }
 
 const styles = StyleSheet.create({
