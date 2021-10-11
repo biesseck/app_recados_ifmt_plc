6c60b2cd889a2fe446bf995235603157da0d52c4
PedroH-OSouza <74661725+PedroH-OSouza@users.noreply.github.com>
2021-10-11 14:56:40 -0400

    Update

--- src/navigation/HomeStack.js
+++ src/navigation/HomeStack.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-
 import HomeScreen from '../screens/HomeScreen';
 
 const Stack = createStackNavigator();
