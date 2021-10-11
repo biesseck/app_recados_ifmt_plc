3237e861028ed0752bb62e3d810b60cacd864e05
PedroH-OSouza <74661725+PedroH-OSouza@users.noreply.github.com>
2021-10-11 14:57:08 -0400

    Update

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-
 import LoginScreen from '../screens/LoginScreen';
 import SignupScreen from '../screens/SignupScreen';
 
