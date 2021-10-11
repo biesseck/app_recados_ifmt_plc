245f545c9a00f9cf354102c302763d438568798e
PedroH-OSouza <74661725+PedroH-OSouza@users.noreply.github.com>
2021-10-11 17:29:52 -0400

    Update GoogleLogin

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -5,6 +5,7 @@ import { StyleSheet, Text, View, Button as RNButton } from 'react-native';
 import { Button, InputField, ErrorMessage } from '../components';
 import {firebase, auth} from '../../config/firebase';
 import SocialButton from '../components/SocialButton';
+import * as Google from 'expo-google-app-auth';
 
 export default function SignupScreen({ navigation }) {
   const [email, setEmail] = useState('');
@@ -33,9 +34,80 @@ export default function SignupScreen({ navigation }) {
     }
   };
 
-  const LoginGoogle = async () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    const result = await auth.signInWithPopup(provider);
+  //const LoginGoogle = async () => {
+    //const provider = new firebase.auth.GoogleAuthProvider();
+    //const result = await auth.signInWithPopup(provider);
+  //}
+
+  function isUserEqual(googleUser, firebaseUser) {
+    if (firebaseUser) {
+      var providerData = firebaseUser.providerData;
+      for (var i = 0; i < providerData.length; i++) {
+        if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+          providerData[i].uid === googleUser.getBasicProfile().getId()) {
+          // We don't need to reauth the Firebase connection.
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  // CONST NÃO FUNCIONOU TESTe COM FUNÇÕES
+  function onSignIn(googleUser) {
+    console.log('Google Auth Response', googleUser);
+    // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+    var unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
+      unsubscribe();
+      // Check if we are already signed-in Firebase with the correct user.
+      if (!isUserEqual(googleUser, firebaseUser)) {
+        // Build Firebase credential with the Google ID token.
+        var credential = firebase.auth.GoogleAuthProvider.credential(
+          googleUser.idToken,
+          googleUser.accessToken,
+        );
+
+        // Sign in with credential from the Google user.
+        firebase.auth().signInWithCredential(credential).catch((error) => {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+        });
+      } else {
+        console.log('User already signed-in Firebase.');
+      }
+    });
+  }
+
+  const signInWithGoogleAsync = async () => {
+    try {
+      const result = await Google.logInAsync({
+        androidClientId: '512511721451-t98qi7pceqkjdeqoakcbj7bgl8e9qj3b.apps.googleusercontent.com',
+        //iosClientId: YOUR_CLIENT_ID_HERE,
+        scopes: ['profile', 'email'],
+      });
+
+      if (result.type === 'success') {
+        onSignIn(result)
+        return result.accessToken;
+
+        const {uid, displayname, photoURL} = result.user;
+        setUsuario({
+          id: uid,
+          avatar: photoURL,
+          name: displayname,
+        })
+
+      } else {
+        return { cancelled: true };
+      }
+    } catch (e) {
+      return { error: true };
+    }
   }
 
   return (
@@ -94,7 +166,7 @@ export default function SignupScreen({ navigation }) {
         btnType="google"
         color="#de4d41"
         backgroundColor="#f5e7ea"
-        onPress={LoginGoogle}
+        onPress={signInWithGoogleAsync}
       />
       <RNButton
         onPress={() => navigation.navigate('Login')}
